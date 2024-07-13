e1a6eb4f6d47fd41576143509cd6b9b03ed8e5c6
Timothy Keitt <tkeitt@utexas.edu>
2024-07-13 12:05:17 -0500

    updated transfer script

--- server_scripts/0piboot/010-config-eeprom.sh
+++ server_scripts/0piboot/010-config-eeprom.sh
@@ -14,19 +14,26 @@ BOOT_ORDER_EXISTS=$?
 if [ $PSU_MAX_CURRENT_EXISTS -ne 0 ] || [ $BOOT_ORDER_EXISTS -ne 0 ]; then
     echo "Updating EEPROM configuration..."
 
-    # Build the new configuration string
-    NEW_CONFIG="$CURRENT_CONFIG"
-    
+    # Create a temporary file for the new configuration
+    TEMP_CONFIG=$(mktemp)
+
+    # Write the current configuration to the temporary file
+    echo "$CURRENT_CONFIG" > "$TEMP_CONFIG"
+
+    # Append necessary lines if they do not exist
     if [ $PSU_MAX_CURRENT_EXISTS -ne 0 ]; then
-        NEW_CONFIG=$(echo "$NEW_CONFIG"; echo "PSU_MAX_CURRENT=5000")
+        echo "PSU_MAX_CURRENT=5000" >> "$TEMP_CONFIG"
     fi
 
     if [ $BOOT_ORDER_EXISTS -ne 0 ]; then
-        NEW_CONFIG=$(echo "$NEW_CONFIG"; echo "BOOT_ORDER=0xf641")
+        echo "BOOT_ORDER=0xf641" >> "$TEMP_CONFIG"
     fi
 
     # Apply the new configuration
-    echo "$NEW_CONFIG" | sudo rpi-eeprom-config --apply
+    rpi-eeprom-config --apply "$TEMP_CONFIG"
+
+    # Clean up the temporary file
+    rm "$TEMP_CONFIG"
 else
     echo "EEPROM configuration is already up to date."
 fi
