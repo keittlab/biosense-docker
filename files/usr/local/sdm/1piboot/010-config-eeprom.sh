f4a5c0887d53a1a3e5d8860316aca146ad3cbf60
Timothy Keitt <tkeitt@gmail.com>
2024-10-30 15:26:05 -0500

    fixed typo

--- files/usr/local/sdm/1piboot/010-config-eeprom.sh
+++ files/usr/local/sdm/1piboot/010-config-eeprom.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
-
-LOGFILE="/var/log/biosense/eeprom-config.log"
 BOOTCONF="/tmp/bootconf.txt"
-
 # Create boot configuration file
 echo '[all]' >"$BOOTCONF"
 echo "BOOT_UART=1" >>"$BOOTCONF"
 echo "POWER_OFF_ON_HALT=1" >>"$BOOTCONF"
 echo "PSU_MAX_CURRENT=5000" >>"$BOOTCONF"
 echo "BOOT_ORDER=0xf461" >>"$BOOTCONF"
-
-# Apply configuration and log output and result
-{
-    echo "Applying EEPROM configuration..."
-    echo "Configuration file content:"
-    cat "$BOOTCONF"
-
-    if sudo rpi-eeprom-config --apply "$BOOTCONF"; then
-        echo "Update command completed successfully."
-    else
-        echo "Update command failed."
-    fi
-} | sudo tee -a "$LOGFILE" | logger -t EEPROM-Update
-
-# Summary logging
-if grep -q "UPDATE SUCCESSFUL" "$LOGFILE"; then
-    echo "$(date): EEPROM configuration applied successfully." | sudo tee -a "$LOGFILE"
-else
-    echo "$(date): EEPROM configuration failed." | sudo tee -a "$LOGFILE"
-fi
+sudo rpi-eeprom-config --apply "$BOOTCONF"
